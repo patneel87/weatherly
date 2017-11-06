d9f1b67ec9b8070cf9440d664fd0f6091af909bd
Rob Morgan <rmorgan323@gmail.com>
2017-11-06 15:01:56 -0700

    testing for app component

--- test/App.test.js
+++ test/App.test.js
@@ -2,36 +2,57 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import App from '../lib/App';
 import LocalStorage from './__test-helpers__/storageMock';
-import './__test-helpers__/mockData.js';
-
+import mockData from '../lib/MockData';
 
 global.fetch = () => {
-
-	return {
-
-	}
+	const l = new Promise((res, rej) => {
+		res({
+			json: function() {
+			return {data: mockData}
+			}
+		});
+	});
+	return l;
 }
 
-
-
-
 describe('App', () => {
+  it('should render search and welcome if there is NO value in local storage', () => {
+    localStorage.clear();
+    const wrapper = shallow(<App />);
 
-   
-
-  it('should render only the search component and welcome component if there is no value in local storage', () => {
-    const app = mount(<App />);
-
-    
-
-    global.localStorage = { localStorage: {city: 'Albuquerque', state: 'NM'}}
-    console.log(global.localStorage)
-
-    // expect(wrapper.find('Search').length).toEqual(1);
-    // expect(wrapper.find('CurrentWeather').length).toEqual(1);
-    // expect(wrapper.find('SevenHourForecast').length).toEqual(1);
-    // expect(wrapper.find('TenDayForecast').length).toEqual(1);
+    expect(wrapper.find('Search').length).toEqual(1);
+    expect(wrapper.find('Welcome').length).toEqual(1);
 
   })
+
+  it('should not render current weather, seven hour forecast, or ten day forecast if there is a NO value in local storage', () => {
+    localStorage.clear();
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('CurrentWeather').length).toEqual(0);
+    expect(wrapper.find('SevenHour').length).toEqual(0);
+    expect(wrapper.find('TenDay').length).toEqual(0);
+  })
+
+
+	it('should render search, current weather, seven hour forecast, and ten day forecast if there is a value in local storage', () => {
+    localStorage.setItem('city', 'Albuquerque');
+    localStorage.setItem('state', 'NM');
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('Search').length).toEqual(1);
+    expect(wrapper.find('CurrentWeather').length).toEqual(1);
+    expect(wrapper.find('SevenHour').length).toEqual(1);
+    expect(wrapper.find('TenDay').length).toEqual(1);
+  })
+
+	it('should not render welcome if there is a value in local storage', () => {
+    localStorage.setItem('city', 'Albuquerque');
+    localStorage.setItem('state', 'NM');
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('Welcome').length).toEqual(0);
+  })
+
 
 })
